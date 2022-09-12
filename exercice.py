6dcd0ae9345f822b23c58e46fe273127a73b39c9
Verocayden <112421679+Verocayden@users.noreply.github.com>
2022-09-12 09:31:19 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
+    nouveau_mot = ""
+    for lettre in mot:
+        nouveau_mot += chr(ord(lettre)-32)
+    mot = nouveau_mot
     return mot
-
 
 if __name__ == '__main__':
     mots = [
